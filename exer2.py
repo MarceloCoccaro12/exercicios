bacb2114fc8c9c061b6ffdc4dba4459b9e6dd491
Marcelo <marcelococcaro@email.com>
2024-03-19 23:04:20 -0300

    Adicionando exemplo 2

---
+++ exer2.py
@@ -0,0 +1,15 @@
+n=int(input("Digite seu código correspondente:"))
+
+if (n==1): #Na parte de declarar os números, evitar colocar em aspas ("")
+    print("Escrituário")
+elif (n==2):
+    print("Secretária")
+elif (n==3):
+    print("Caixa")
+elif (n==4):
+    print("Gerente")
+elif (n==5):
+    print("Diretor")
+else:
+    print("Código errado")
+
